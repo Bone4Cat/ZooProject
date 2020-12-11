c38ffff0c244799e34ad7cd4492b634f39fd046e
ducanhxh <62750101+ducanhxh@users.noreply.github.com>
2020-12-11 02:05:25 +0200

    Add files via upload

--- models/animal.py
+++ models/animal.py
@@ -1,31 +1,36 @@
-from extensions import db
-
 animal_list = []
 
 
 def get_last_id():
     if animal_list:
         last_animal = animal_list[-1]
-
-
     else:
         return 1
     return last_animal.id + 1
 
-class Animal(db.Model):
-    __tablename__ = 'animal'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    species = db.Column(db.String(200))
-    gender = db.Column(db.String(200))
-    birthdate = db.Column(db.DateTime())
-    date_aquired = db.Column(db.DateTime())
-    location = db.Column(db.String(200))
-    keeper = db.Column(db.String(200))
 
-    is_publish = db.Column(db.Boolean(), default=False)
-    created_at = db.Column(db.DateTime(), nullable=False, server_default = db.func.now())
+class Animal:
 
-    updated_at = db.Column(db.DateTime(), nullable=False, server_default = db.func.now(), onupdate = db.func.now())
+    def __init__(self, name, species, gender, birthdate, date_acquired, location, keeper):
+        self.id = get_last_id()
+        self.name = name
+        self.species = species
+        self.gender = gender
+        self.birthdate = birthdate
+        self.date_acquired = date_acquired
+        self.location = location
+        self.keeper = keeper
+        self.is_publish = False
 
-    user_id = db.Column(db.Integer(), db.ForeignKey("personel.id"))
+    @property
+    def data(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'species': self.species,
+            'gender': self.gender,
+            'birthdate': self.birthdate,
+            'date_acquired': self.date_acquired,
+            'location': self.location,
+            'keeper': self.keeper
+        }

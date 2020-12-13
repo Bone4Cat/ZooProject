ba1bcfc33a0e529c016749564eacdb5699aab1f2
Peter Ståhlström <peter.stahlstrom@edu.turkuamk.fi>
2020-12-13 20:23:53 +0200

    Added login token creation, refresh, and revocation
    This satisfies task criteria of *reading* from the personel database as well as a resource that requires login.

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ from flask_migrate import Migrate
 from flask_restful import Api
 
 from Config import Config
-from extensions import db
+from extensions import db, jwt
 
 from resources.facility import FacilityListResource, FacilityResource, FacilityPublishResource
 from resources.animal import AnimalListResource, AnimalResource, AnimalPublishResource
 from resources.personel import PersonelListResource
+from resources.token import TokenResource, RefreshResource, RevokeResource, black_list
+
 
 def create_app():
     app = Flask(__name__)
@@ -17,16 +19,23 @@ def create_app():
     register_resources(app)
     return app
 
+
 def register_extensions(app):
     db.init_app(app)
     migrate = Migrate(app, db)
+    jwt.init_app(app)
+
+    @jwt.token_in_blacklist_loader
+    def chek_if_token_in_blacklist(decrypted_token):
+        jti = decrypted_token['jti']
+        return jti in black_list
+
 
 def register_resources(app):
 
     api = Api(app)
 
     api.add_resource(PersonelListResource, '/personels')
-
 
     api.add_resource(FacilityListResource, '/facilities')
     api.add_resource(FacilityResource, '/facilities/<int:facility_id>')
@@ -35,6 +44,10 @@ def register_resources(app):
     api.add_resource(AnimalListResource, '/animals')
     api.add_resource(AnimalResource, '/animals/<int:animal_id>')
     api.add_resource(AnimalPublishResource, '/animals/<int:animal_id>/publish')
+
+    api.add_resource(TokenResource, '/token')
+    api.add_resource(RefreshResource, '/refresh')
+    api.add_resource(RevokeResource, '/revoke')
 
 
 if __name__ == '__main__':
